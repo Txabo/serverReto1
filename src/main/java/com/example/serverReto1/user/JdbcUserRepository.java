3991127f8ab9b19585413f918e1b06521cd1ccc6
Estíbaliz Martín <estibalizmares@gmail.com>
2022-11-08 18:46:36 +0100

    Change JdbcUserRepository class name

--- src/main/java/com/example/serverReto1/user/JdbcUserRepository.java
+++ src/main/java/com/example/serverReto1/user/JdbcUserRepository.java
@@ -6,11 +6,10 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class jdbcUserRepository implements UserRepository{
+public class JdbcUserRepository implements UserRepository{
     @Autowired
     private JdbcTemplate jdbcTemplate;
     @Override
