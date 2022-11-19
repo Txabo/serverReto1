300d88ffa03567fe4f92eaaa2e6b11745a04f39c
DESKTOP-O72PH1Q\Esti <estibalizmares@gmail.com>
2022-11-19 11:46:12 +0100

    Remove warnings (on going)

--- src/main/java/com/example/serverReto1/user/User.java
+++ src/main/java/com/example/serverReto1/user/User.java
@@ -1,6 +1,5 @@
 package com.example.serverReto1.user;
 
-import jdk.jfr.Enabled;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
--- src/main/java/com/example/serverReto1/user/UserService.java
+++ src/main/java/com/example/serverReto1/user/UserService.java
@@ -4,8 +4,8 @@ import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.RequestBody;
 public interface UserService {
-    public int signUp(@Valid @RequestBody User user);
-    /*public List<String> getUserNames();*/
-    public boolean logUser(@RequestBody String userName, String password);
-    public boolean changeUserPassword(PasswordPostRequest passwordPostRequest);
+    int signUp(@Valid @RequestBody User user);
+    /*List<String> getUserNames();*/
+    boolean logUser(@RequestBody String userName, String password);
+    boolean changeUserPassword(PasswordPostRequest passwordPostRequest);
 }
